7d6190dde2e4cb960b428061cfd4115e97c85b26
Bestbatazaaa <Bestbatazaaa@icloud.com>
2024-01-01 13:41:38 +0700

    lab12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int i){
+    if(i == 0){
+        return i;
+    }else if(i == 1){
+        return i;
+    }else{
+        return fibonacci(i-1)+fibonacci(i-2);
+    }
 }
